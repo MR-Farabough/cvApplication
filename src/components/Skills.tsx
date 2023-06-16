5445e0e6d670ab870d41548e18d26148787a2981
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-06-16 18:04:32 -0500

    Basic outline for skills

---
+++ src/components/Skills.tsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+export class Skills extends Component {
+	render() {
+		return (
+			<>
+				<h3 className="header">Skills</h3>
+			</>
+		);
+	}
+}
+
+export default Skills;
