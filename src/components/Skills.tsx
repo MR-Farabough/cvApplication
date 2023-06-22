44a3c384399552f35cdfac670cb2ee5d198f9fda
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-06-22 11:39:56 -0500

    convert to function componenet

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,37 +1,44 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
-export class Skills extends Component {
-	state = {
+interface State {
+	taskArray: string[];
+	renderTask: boolean;
+}
+
+export const Skills = () => {
+	const preState: State = {
 		taskArray: [],
 		renderTask: false,
 	};
-	formValidation() {
+	let [state, setState] = useState(preState);
+	const formValidation = () => {
 		const inputEl: HTMLInputElement | null =
 			document.querySelector('.task-input');
 		const newArray = [];
-		this.state.taskArray.forEach((task) => {
+		state.taskArray.forEach((task) => {
 			newArray.push(task);
 		});
 		if (inputEl && inputEl.value.length > 2) {
 			inputEl.style.border = '1px solid green';
 			newArray.push(inputEl.value);
 			inputEl.value = '';
-			this.setState({
+			const newState = {
 				taskArray: newArray,
 				renderTask: true,
-			});
+			};
+			setState(newState);
 			return true;
 		} else if (inputEl && inputEl.value.length < 2) {
 			inputEl.style.border = '1px solid red';
 			alert('Must be valid skill');
 			return false;
 		}
-	}
-	handleClick() {
-		if (this.formValidation()) {
+	};
+	const handleClick = () => {
+		if (formValidation()) {
 		}
-	}
-	handleDelete(key: string) {
+	};
+	const handleDelete = (key: string) => {
 		let text = document.querySelector(`.a${key}`)?.textContent;
 		let count = 0;
 		if (text) {
@@ -41,47 +48,49 @@ export class Skills extends Component {
 				count++;
 			}
 			const newState: string[] = [];
-			this.state.taskArray.forEach((task) => {
+			state.taskArray.forEach((task) => {
 				if (task != newText) {
 					newState.push(task);
 				}
 			});
-			this.setState({ taskArray: newState });
+			const cacheState = {
+				taskArray: newState,
+				renderTask: state.renderTask,
+			};
+			setState(cacheState);
 		}
-	}
-	render() {
-		return (
-			<>
-				<h3 className="header">Skills</h3>
-				<div>
-					<button onClick={() => this.handleClick()}>Add Task</button>
-				</div>
-				<div className="body-section">
-					<input
-						type="text"
-						className="task-input long-input"
-						placeholder="Enter one skill at a time"
-					/>
-					{this.state.renderTask &&
-						this.state.taskArray.map((task, index) => {
-							return (
-								<div className={`a${index}`} key={index}>
-									<fieldset>
-										<div className="task">{task}</div>
-										<button
-											onClick={() => this.handleDelete(`${index}`)}
-											className="delete-button"
-										>
-											&#10008;
-										</button>
-									</fieldset>
-								</div>
-							);
-						})}
-				</div>
-			</>
-		);
-	}
-}
+	};
+	return (
+		<>
+			<h3 className="header">Skills</h3>
+			<div>
+				<button onClick={() => handleClick()}>Add Task</button>
+			</div>
+			<div className="body-section">
+				<input
+					type="text"
+					className="task-input long-input"
+					placeholder="Enter one skill at a time"
+				/>
+				{state.renderTask &&
+					state.taskArray.map((task, index) => {
+						return (
+							<div className={`a${index}`} key={index}>
+								<fieldset>
+									<div className="task">{task}</div>
+									<button
+										onClick={() => handleDelete(`${index}`)}
+										className="delete-button"
+									>
+										&#10008;
+									</button>
+								</fieldset>
+							</div>
+						);
+					})}
+			</div>
+		</>
+	);
+};
 
 export default Skills;
--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import '../styles/Work.css';
 
 type WorkExperience = {
@@ -15,23 +15,24 @@ interface State {
 	work: WorkExperience[];
 }
 
-export class Work extends Component<{}, State> {
-	state = {
+export const Work = () => {
+	const preState: State = {
 		count: 0,
 		renderReady: false,
 		work: [],
 	};
-	handleOpen = () => {
+	let [state, setState] = useState(preState);
+	const handleOpen = () => {
 		const modal = document.querySelector('.modal-deactive');
 		modal?.classList.remove('modal-deactive');
 		modal?.classList.add('modal-active');
 	};
-	handleClose = () => {
+	const handleClose = () => {
 		const modal = document.querySelector('.modal-active');
 		modal?.classList.remove('modal-active');
 		modal?.classList.add('modal-deactive');
 	};
-	handleSubmit = () => {
+	const handleSubmit = () => {
 		const company: HTMLInputElement | null =
 			document.querySelector('.company-input');
 		const job: HTMLInputElement | null = document.querySelector('.job-input');
@@ -56,7 +57,7 @@ export class Work extends Component<{}, State> {
 				startDate.style.border = '1px solid green';
 				endDate.style.border = '1px solid green';
 
-				const newJobArray: WorkExperience[] = [...this.state.work];
+				const newJobArray: WorkExperience[] = [...state.work];
 				newJobArray.push({
 					companyName: company.value,
 					jobTitle: job.value,
@@ -64,18 +65,17 @@ export class Work extends Component<{}, State> {
 					startDate: startDate.value,
 					endDate: endDate.value,
 				});
-
-				this.setState({
-					count: this.state.count + 1,
+				const newState = {
+					count: state.count + 1,
 					renderReady: true,
 					work: newJobArray,
-				});
-
-				this.handleClose();
+				};
+				setState(newState);
+				handleClose();
 				company.value = '';
 				job.value = '';
 				responsibilies.value = '';
-				console.log(this.state);
+				console.log(state);
 			} else {
 				company.style.border = '1px solid red';
 				job.style.border = '1px solid red';
@@ -85,15 +85,18 @@ export class Work extends Component<{}, State> {
 			}
 		}
 	};
-	handleDelete = (index: number) => {
-		const newWorkArray: WorkExperience[] = [...this.state.work];
+	const handleDelete = (index: number) => {
+		const newWorkArray: WorkExperience[] = [...state.work];
 		newWorkArray.splice(index, 1);
-		this.setState({
+		const newState = {
+			count: state.count,
+			renderReady: state.renderReady,
 			work: newWorkArray,
-		});
+		};
+		setState(newState);
 	};
-	handleWorkExperience() {
-		return this.state.work.map((prevJob: WorkExperience, index: number) => (
+	const handleWorkExperience = () => {
+		return state.work.map((prevJob: WorkExperience, index: number) => (
 			<div className="work-card" key={index}>
 				<h2 className="work-title">{prevJob.companyName}</h2>
 				<h3 className="work-position">{prevJob.jobTitle}</h3>
@@ -101,66 +104,59 @@ export class Work extends Component<{}, State> {
 				<h4 className="work-dates">
 					Start - {prevJob.startDate} || End - {prevJob.endDate}
 				</h4>
-				<button onClick={() => this.handleDelete(index)}>Delete</button>
+				<button onClick={() => handleDelete(index)}>Delete</button>
 			</div>
 		));
-	}
-
-	render() {
-		return (
-			<>
-				<div className="backdrop"></div>
-				<h3 className="header">Work</h3>
-				<div>
-					<button onClick={this.handleOpen}>Add Work Experience</button>
-				</div>
-				<div className="body-section">
-					<div className="modal modal-deactive">
-						<button onClick={this.handleClose}>&#10008;</button>
-						<div>
-							<h3>Company Name</h3>
-							<input
-								type="text"
-								placeholder="Company Name"
-								className="company-input"
-							/>
-						</div>
-						<div>
-							<h3>Job Title</h3>
-							<input
-								type="text"
-								placeholder="Job Title"
-								className="job-input"
-							/>
-						</div>
-						<div>
-							<h3>Responsibilities</h3>
-							<input
-								type="text"
-								placeholder="Separate with a comma"
-								className="responsibilites-input"
-							/>
-						</div>
-						<div>
-							<h3>Length of Employment</h3>
-							<div className="date-input">
-								<div>
-									<h5>Start</h5>
-									<input className="start-date-input" type="date" />
-								</div>
-								<div>
-									<h5>End</h5>
-									<input className="end-date-input" type="date" />
-								</div>
+	};
+	return (
+		<>
+			<div className="backdrop"></div>
+			<h3 className="header">Work</h3>
+			<div>
+				<button onClick={handleOpen}>Add Work Experience</button>
+			</div>
+			<div className="body-section">
+				<div className="modal modal-deactive">
+					<button onClick={handleClose}>&#10008;</button>
+					<div>
+						<h3>Company Name</h3>
+						<input
+							type="text"
+							placeholder="Company Name"
+							className="company-input"
+						/>
+					</div>
+					<div>
+						<h3>Job Title</h3>
+						<input type="text" placeholder="Job Title" className="job-input" />
+					</div>
+					<div>
+						<h3>Responsibilities</h3>
+						<input
+							type="text"
+							placeholder="Separate with a comma"
+							className="responsibilites-input"
+						/>
+					</div>
+					<div>
+						<h3>Length of Employment</h3>
+						<div className="date-input">
+							<div>
+								<h5>Start</h5>
+								<input className="start-date-input" type="date" />
+							</div>
+							<div>
+								<h5>End</h5>
+								<input className="end-date-input" type="date" />
 							</div>
 						</div>
-						<button onClick={this.handleSubmit}>Submit</button>
 					</div>
-					{this.state.renderReady && this.handleWorkExperience()}
+					<button onClick={handleSubmit}>Submit</button>
 				</div>
-			</>
-		);
-	}
-}
+				{state.renderReady && handleWorkExperience()}
+			</div>
+		</>
+	);
+};
 
 export default Work;
